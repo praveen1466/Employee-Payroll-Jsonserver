3f3f06003aa30787368a828cf3d27450328bb472
praveen1466 <praveensknl7@gmail.com>
2020-12-30 22:40:14 +0530

    Added: to set event listeners when document is loaded

--- js/EmployeePayroll.js
+++ js/EmployeePayroll.js
@@ -1,34 +1,32 @@
-// Usecase 8
-const salary = document.querySelector('#salary');
-const output = document.querySelector('.salary-output');
-output.textContent = salary.value;
-salary.addEventListener('input', function(){
-    output.textContent = salary.value;
-});
-
-//Usecase 10
-const text = document.querySelector('#name');
-const textError = document.querySelector('.text-error');
-text.addEventListener('input', function(){
-    let nameRegex = RegExp('^[A-Z]{1}{a-z}{2,}$');
-    if(nameRegex.test(text.value)){
-        textError.textContent = "";
-    }
-    else{
-        textError.textContent = "Name is Incorrect";
-    }
-});
-
 const notes = document.querySelector('#notes');
 const notesError = document.querySelector('.notes-error');
 notes.addEventListener('input', function(){
-    let notesRegex = RegExp('^{a-z}{2,}$');
-    if(notesRegex.test(notes.value)){
-        notesError.textContent = "";
+    if(notes.value.length < 3){
+        notesError.textContent = "It should be minimum 3 character";
     }
     else{
-        notesError.textContent = "It should be minimum 3 character";
+        notesError.textContent = "";
     }
 });
 
+window.addEventListener('DOMContentLoaded', (event) => {
+    const text = document.querySelector('#name');
+    const textError = document.querySelector('.text-error');
+    text.addEventListener('input', function(){
+        if(text.value.length < 3){
+            textError.textContent = "It should be atleast 3 characters";
+        }
+        else{
+            textError.textContent = "";
+        }
+    });
 
+    // Usecase 8
+    const salary = document.querySelector('#salary');
+    const output = document.querySelector('.salary-output');
+    output.textContent = salary.value;
+    salary.addEventListener('input', function(){
+        output.textContent = salary.value;
+    });
+
+});
