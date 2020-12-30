3069dd56ced1d38b38f0903d23e62df78cfd0bd2
praveen1466 <praveensknl7@gmail.com>
2020-12-30 22:05:32 +0530

    Added: Employee payroll with new attributes and getters and setters

--- js/EmployePayrollObjectData.js
+++ js/EmployePayrollObjectData.js
@@ -24,49 +24,49 @@ class EmployeePayRollData{
 
     //getter and setter method
     get name(){
-    return this.name;
+    return this._name;
     }
     set name(name){
         this._name=name;
     }
     
     get profilPic(){
-        return this.profilPic;
+        return this._profilPic;
     }
     set profilPic(profilPic){
         this._profilPic=profilPic;
     }
      
     get gender(){
-        return this.gender;
+        return this._gender;
     }
     set gender(gender){
         this._gender=gender;
     }
 
     get department(){
-        return this.department;
+        return this._department;
     }
     set department(department){
         this._department=department;
     }
 
     get salary(){
-        return this.salary;
+        return this._salary;
     }
     set salary(salary){
         this._salary=salary;
     }
 
     get startDate(){
-        return this.startDate;
+        return this._startDate;
     }
     set startDate(startDate){
         this._startDate=startDate;
     }
 
     get notes(){
-        return this.notes;
+        return this._notes;
     }
     set notes(notes){
         this._notes=notes;
@@ -76,7 +76,7 @@ class EmployeePayRollData{
 
         const options = { year: 'numeric', month: 'short', day: 'numeric' };
         const empDate = !this.startDate ? "undefined" :
-            this.startDate;
+            this.startDate.toLocaleDateString("en-US",options);
         return "id=" + this.id + ", name='" + this.name + ", gender='" + this.gender +
             ", profilePic='" + this.profilePic + ", department=" + this.department +
             ", salary=" + this.salary + ", startDate=" + empDate + ", notes=" + this.notes;
