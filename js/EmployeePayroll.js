03920e26fd18d47213c58fde5db925395aa24728
praveen1466 <praveensknl7@gmail.com>
2020-12-31 09:59:04 +0530

    Added: reset the employee payroll form

--- js/EmployeePayroll.js
+++ js/EmployeePayroll.js
@@ -98,17 +98,32 @@ function createAndUpdateStorage(employeePayrollData){
     localStorage.setItem("EmployeePayrollList", JSON.stringify(employeePayrollList));
 }
 
+const resetForm = () =>{
+    setValue('#name','');
+    unsetSelectedValues('[name=profile]');
+    unsetSelectedValues('[name=gender]');
+    unsetSelectedValues('[name=department]');
+    setValue('#salary','');
+    setValue('#note','');
+    setValue('#day','1');
+    setValue('#month','January');
+    setValue('#year','2020');
 
+}
 
+const unsetSelectedValues = (propertyValue) => {
+    let allItems = document.querySelectorAll(propertyValue);
+    allItems.forEach(item => {
+        item.checked = false;
+    });
+}
 
+const setTextValue = (id, value) => {
+    const element = document.querySelector(id);
+    element.textContent = value;
+}
 
-
-
-
-
-
-
-
-
-
-
+const setValue = (id, value) => {
+    const element = document.querySelectorAll(id);
+    element.value = value;
+}
