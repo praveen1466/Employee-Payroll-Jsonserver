207be7944c2fcefd89554de0e23d99cba4c59ac4
praveen1466 <praveensknl7@gmail.com>
2021-01-02 22:17:48 +0530

    Added: Display employee details from local storage

--- js/Homepage.js
+++ js/Homepage.js
@@ -1,43 +1,13 @@
+let empPayrollList;
 window.addEventListener('DOMContentLoaded', (event) => {
+    empPayrollList = getEmployeePayrollDataFromStorage();
+    document.querySelector(".emp-count").textContent = empPayrollList.length;
     createInnerHtml();
+    localStorage.removeItem('editEmp');
 });
 
-const createInnerHtml = () => {
-
-    const headerHtml = "<th></th><th>Name</th><th>Gender</th><th>Department</th>"+
-                        "<th>Salary</th><th>Start Date</th><th>Actions</th>";
-
-    let innerHtml = `${headerHtml}`;
-    const empPayrollList = createEmployeePayrollJSON();
-    for(const empPayrollData of empPayrollList){
-        innerHtml = `${innerHtml}
-        <tr>
-            <td><img class="profile" src="${empPayrollData._profilePic}" alt=""></td>
-            <td>${empPayrollData._name}</td>
-            <td>${empPayrollData._gender}</td>
-            <td>
-                <div class="dept-label">${empPayrollData._department[0]}</div>
-                <div class="dept-label">${empPayrollData._department[1]}</div>
-            </td>
-                <div class="dept-label">${empPayrollData._department[0]}</div>
-            <td>${empPayrollData._salary}</td>
-            <td>${empPayrollData._startDate}</td>
-            <td>
-                <img name="${empPayrollData._id}" onclick="remove(this)" src="../assets/delete.png" alt="delete">
-                <img name="${empPayrollData._id}" onclick="update(this)" src="../assets/edit.png" alt="edit">
-            </td>
-        </tr>
-    `;
-    }
-    document.querySelectorAll('#table-display').innerHtml = innerHtml;
-}
-
-const getDeptHtml = (deptList) => {
-    let deptHtml = '';
-    for(const dept of deptList){
-        deptHtml = `${deptHtml} <div class='dept-label'>${dept}</div>`
-    }
-    return deptHtml;
+const getEmployeePayrollDataFromStorage = () => {
+    return localStorage.getItem('EmployeePayrollList') ? JSON.parse(localStorage.getItem('EmployeePayrollList')) : [];
 }
 
 const createEmployeePayrollJSON = () => {
@@ -71,3 +41,41 @@ const createEmployeePayrollJSON = () => {
     ];
     return empPayrollListLocal;
 }
+
+const createInnerHtml = () => {
+    const headerHtml = "<th></th><th>Name</th><th>Gender</th><th>Department</th>"+
+                        "<th>Salary</th><th>Start Date</th><th>Actions</th>";
+
+    if (empPayrollList.length == 0) return;
+    let innerHtml = `${headerHtml}`;
+    for(const empPayrollData of empPayrollList){
+        innerHtml = `${innerHtml}
+        <tr>
+            <td><img class="profile" src="${empPayrollData._profilePic}" alt=""></td>
+            <td>${empPayrollData._name}</td>
+            <td>${empPayrollData._gender}</td>
+            <td>
+                <div class="dept-label">${empPayrollData._department[0]}</div>
+                <div class="dept-label">${empPayrollData._department[1]}</div>
+            </td>
+                <div class="dept-label">${empPayrollData._department[0]}</div>
+            <td>${empPayrollData._salary}</td>
+            <td>${empPayrollData._startDate}</td>
+            <td>
+                <img name="${empPayrollData._id}" onclick="remove(this)" src="../assets/delete.png" alt="delete">
+                <img name="${empPayrollData._id}" onclick="update(this)" src="../assets/edit.png" alt="edit">
+            </td>
+        </tr>
+    `;
+    }
+    document.querySelector('#table-display').innerHtml = innerHtml;
+}
+
+const getDeptHtml = (deptList) => {
+    let deptHtml = '';
+    for(const dept of deptList){
+        deptHtml = `${deptHtml} <div class='dept-label'>${dept}</div>`
+    }
+    return deptHtml;
+}
+
