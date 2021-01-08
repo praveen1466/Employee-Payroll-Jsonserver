e25700d436e1accb12ad8bb771385b7e1ae2b4d7
praveen1466 <praveensknl7@gmail.com>
2021-01-08 18:10:06 +0530

    Added: populate employee payroll class with json object and save to local storage

--- js/EmployeePayroll.js
+++ js/EmployeePayroll.js
@@ -100,16 +100,27 @@ let isUpdate = false;
 let employeePayrollObj = {};
 window.addEventListener('DOMContentLoaded', (event) => {
     const name = document.querySelector("#name")
-    const textError = document.querySelector('.text-error')
     name.addEventListener('input', function() {
         if (name.value.length == 0) {
-            textError.textContent = ""
+            setTextValue('.text-error', "");
             return
         }
         try {
-            (new EmployeePayRoll).name = name.value
-            textError.textContent = ""
-        } catch (e) { textError.textContent = e }
+            (new EmployeePayRollData()).name = name.value;
+            setTextValue('.text-error', "");
+        } catch (e) { setTextValue('.text-error', e); }
+    });
+
+    const date = document.querySelector('#date');
+    date.addEventListener('input', function(){
+        let startDate = getInputValueById('#day')+" "+getInputValueById('#month')+" "+getInputValueById('#year');
+        try{
+            (new EmployeePayRollData()).startDate = new Date(Date.parse(startDate));
+            setTextValue('.date-error', "");
+        }
+        catch(e){
+            setTextValue('.date-error', e);
+        }
     });
 
     //UC8: Adding event listener on salary range is added
@@ -125,13 +136,28 @@ window.addEventListener('DOMContentLoaded', (event) => {
 
 
 //UC14: saving the object to the local storage on submit
-const save = () => {
+const save = (event) => {
+    event.preventDefault();
+    event.stopPropagation();
     try {
-        let employeePayrollData = createEmployeePayroll();
-        createAndUpdateStorage(employeePayrollData);
+        setEmployeePayrollObject();
+        createAndUpdateStorage();
+        resetForm();
+        window.location.replace(site_properties.Homepage);
     } catch (e) {
         return;
     }
+}
+
+const setEmployeePayrollObject = () => {
+    employeePayrollObj._name = getInputValueById('#name');
+    employeePayrollObj._profilePic = getSelectedValues('[name=profile]').pop();
+    employeePayrollObj._profilePic = getSelectedValues('[name=profile]').pop();
+    employeePayrollObj._profilePic = getSelectedValues('[name=profile]').pop();
+    employeePayrollObj._salary = getInputValueById('#salary');
+    employeePayrollObj._note = getInputValueById('#note');
+    let date = getInputValueById('#day')+" "+getInputValueById('#month')+" "+getInputValueById('#year');
+    employeePayrollObj._startDate = date;
 }
 
 const createEmployeePayroll = () => {
@@ -158,8 +184,15 @@ const createEmployeePayroll = () => {
 function createAndUpdateStorage(employeePayrollData){
     let employeePayrollList = JSON.parse(localStorage.getItem("EmployeePayrollList"));
 
-    if(employeePayrollList != undefined){
-        employeePayrollList.push(employeePayrollData);
+    if(employeePayrollList){
+        let empPayrollData = employeePayrollList.find(empData => empData._id == employeePayrollObj._id );
+        if(!empPayrollData){
+            employeePayrollList.push(createEmployeePayrollData());
+        }
+        else{
+            const index = employeePayrollList.map(empData => empData._id).indexOf(empPayrollData._id);
+            employeePayrollList.splice(index, 1, createEmployeePayrollData(empPayrollData._id));
+        }
     }
     else{
         employeePayrollList = [employeePayrollData]
@@ -168,6 +201,45 @@ function createAndUpdateStorage(employeePayrollData){
     alert(employeePayrollList.toString());
     localStorage.setItem("EmployeePayrollList", JSON.stringify(employeePayrollList));
 }
+
+
+const createEmployeePayrollData = () => {
+    let employeePayrollData = new EmployeePayRollData();
+    if(!id) employeePayrollData.id = createNewEmployeeId();
+    else employeePayrollData.id = id;
+    setEmployeePayrollData(employeePayrollData);
+    return employeePayrollData;
+}
+
+const setEmployeePayrollData = (employeePayrollData) => {
+    try{
+        employeePayrollData.name = employeePayrollObj._name;
+    }
+    catch(e){
+        setTextValue('.text-error', e);
+    }
+
+    employeePayrollData.profilePic = employeePayrollObj._profilePic
+    employeePayrollData.gender = employeePayrollObj._gender
+    employeePayrollData.department = employeePayrollObj._department
+    employeePayrollData.salary = employeePayrollObj._salary
+    employeePayrollData.note = employeePayrollObj._note
+    try{
+        employeePayrollData.startDate = new Date(Date.parse(employeePayrollObj._startDate));
+    }
+    catch(e){
+        setTextValue('.text-error', e);
+        throw e;
+    }
+    alert(employeePayrollData.toString());
+}
+
+const createNewEmployeeId = () => {
+    let empID = localStorage.getItem("EmployeeID");
+    empID = !empID ? 1 : (parseInt(empID)+1).toString();
+    localStorage.setItem("EmployeeID", empID);
+    return empID
+;}
 
 const getSelectedValues = (propertyValue) => {
     let allItems = document.querySelectorAll(propertyValue);
--- js/Homepage.js
+++ js/Homepage.js
@@ -87,4 +87,93 @@ const createEmployeePayrollJSON = () => {
         }
     ];
     return empPayrollListLocal;
+}let empPayrollList;
+window.addEventListener('DOMContentLoaded', (event) => {
+    empPayrollList = getEmployeePayrollDataFromStorage();
+    document.querySelector(".emp-count").textContent = empPayrollList.length;
+    createInnerHtml();
+    localStorage.removeItem('editEmp');
+});
+
+const getEmployeePayrollDataFromStorage = () => {
+    return localStorage.getItem('EmployeePayrollList') ? JSON.parse(localStorage.getItem('EmployeePayrollList')) : [];
+}
+
+//Template literals ES6 feature
+const createInnerHtml = () => {
+    if(empPayrollList.length == 0) return;
+    const headerHtml = "<th></th><th>Name</th><th>Gender</th><th>Department</th>"+
+                        "<th>Salary</th><th>Start Date</th><th>Actions</th>";
+
+    
+    let innerHtml = `${headerHtml}`;
+    let empPayrollList = createEmployeePayrollJSON();
+    for(const empPayrollData of empPayrollList){
+    innerHtml = `${innerHtml}
+        <tr>
+            <td><img class="profile" src="${empPayrollData._profilePic}" alt=""></td>
+            <td>${empPayrollData._name}</td>
+            <td>${empPayrollData._gender}</td>
+            <td>${getDeptHtml(empPayrollData._department)}</td>
+            <td>${empPayrollData._salary}</td>
+            <td>${empPayrollData._startDate}</td>
+            <td>
+                <img id="${empPayrollData._id}" onclick="remove(this)" src="../assets/delete.png" alt="delete">
+                <img id="${empPayrollData._id}" onclick="update(this)" src="../assets/edit.png" alt="edit">
+            </td>
+        </tr>
+    `;
+    }
+    document.querySelector('#table-display').innerHTML = innerHtml;
+}
+
+const getDeptHtml = (deptList) => {
+    let deptHtml = '';
+    for(const dept of deptList){
+        deptHtml = `${deptHtml} <div class='dept-label'>${dept}</div>`
+    }
+    return deptHtml;
+}
+
+
+const remove = (node) => {
+    let empPayrollData = empPayrollList.find(empData => empData._id == node.id);
+    if(!empPayrollData) return;
+    const index = empPayrollList.map(empData => empData._id).indexOf(empPayrollData._id);
+    empPayrollList.splice(index,1);
+    localStorage.setItem("EmployeePayrollList", JSON.stringify(empPayrollList));
+    createInnerHtml();
+}
+
+
+const createEmployeePayrollJSON = () => {
+    let empPayrollListLocal = [
+        {
+            _name: 'Praveen Sakinala',
+            _gender: 'male',
+            _department: [
+                'Engineering',
+                'Finance'
+            ],
+            _salary: '400000',
+            _startDate: '29 Oct 2019',
+            _note: '',
+            _id: new Date().getTime(),
+            _profilePic: '../assets/profile2.jpg'
+        },
+        {
+            _name: 'Shiva Shankar',
+            _gender: 'male',
+            _department: [
+                'HR',
+                'Sales'
+            ],
+            _salary: '350000',
+            _startDate: '19 Nov 2019',
+            _note: '',
+            _id: new Date().getTime(),
+            _profilePic: '../assets/profile4.jpg'
+        }
+    ];
+    return empPayrollListLocal;
 }
